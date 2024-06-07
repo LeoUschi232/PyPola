06226990f606065a27b915925924aff204082fac
Leonard Uscinowicz <leouscin@gmail.com>
2024-06-07 14:27:44 +0200

    Made plot of static no compensation meassurement

--- Analysis/Simulations/analysis_fiber_no_compensation.py
+++ Analysis/Simulations/analysis_fiber_no_compensation.py
@@ -1,8 +1,9 @@
-from PyPola.FiberNetworkComponents.optical_fiber import OpticalFiber
+from PyPola.FiberNetworkComponents.optical_fiber import OpticalFiber, load_fiber_from_csv
 from PyPola.FiberNetworkComponents.OpticalInstruments.polarization_beam_splitter import PolarizationBeamSplitter
 from PyPola.utilities.stokes_vector import StokesVector
 from numpy import pi, arange
 from matplotlib import pyplot as plt
+from os import path
 
 horizontal_sv = StokesVector(s0=1, s1=1, s2=0, s3=0)
 vertical_sv = StokesVector(s0=1, s1=-1, s2=0, s3=0)
@@ -12,88 +13,110 @@ pbs = PolarizationBeamSplitter()
 
 # The fiber is assumed to be a single-mode fiber.
 # Fiber property combinations to iterate over:
-fiber_lengths = [1000, 10000, 100000]
-fiber_theta_fluctuations = [pi / (2 ** i) for i in range(8, 4, -1)]
-fiber_delta_fluctuations = [pi / (2 ** i) for i in range(8, 4, -1)]
-combined_fiber_properties = [
-    [fiber_length, fiber_theta_fluctuation, fiber_delta_fluctuation]
-    for fiber_length in fiber_lengths
-    for fiber_theta_fluctuation in fiber_theta_fluctuations
-    for fiber_delta_fluctuation in fiber_delta_fluctuations
-]
+fiber_lengths = [1000, 10000, 100000, 1000000]
+theta_fluctuations = [(pi / (2 ** i), i) for i in range(8, 4, -1)]
+delta_fluctuations = [(pi / (2 ** i), i) for i in range(8, 4, -1)]
 
 # Lists to save the QBIT error rates for each fiber property combination
 # combinations = len(fiber_lengths) * len(fiber_theta_fluctuations) * len(fiber_delta_fluctuations)
-nr_of_datapoints = 5
 horizontal_qbers = []
 vertical_qbers = []
 diagonal_qbers = []
 antidiagonal_qbers = []
 
-for i in range(len(combined_fiber_properties)):
-    fiber_length, fiber_theta_fluctuation, fiber_delta_fluctuation = combined_fiber_properties[i]
-    horizontal_qber = 0
-    vertical_qber = 0
-    diagonal_qber = 0
-    antidiagonal_qber = 0
-    for _ in range(nr_of_datapoints):
-        print(f"No compensation fiber analysis progress {i + 1}/{len(combined_fiber_properties)}")
-        fiber = OpticalFiber(
-            nr_of_segments=fiber_length,
-            temporal_pmd_theta_fluctuation=fiber_theta_fluctuation,
-            temporal_pmd_delta_fluctuation=fiber_delta_fluctuation
-        )
+max_progress = len(fiber_lengths) * len(theta_fluctuations) * len(delta_fluctuations)
+progress = 0
+for fiber_length in fiber_lengths:
+    for fiber_theta_fluctuation, i_t in theta_fluctuations:
+        for fiber_delta_fluctuation, i_d in delta_fluctuations:
+            horizontal_qber = 0
+            vertical_qber = 0
+            diagonal_qber = 0
+            antidiagonal_qber = 0
+            v = 0
+            while True:
+                fiber_path = f"Fibers/FibersLength{fiber_length}/fiber_n{fiber_length}_t{i_t}_d{i_d}_v{v}.csv"
+                if not path.exists(fiber_path):
+                    if v > 0:
+                        horizontal_qber /= v
+                        vertical_qber /= v
+                        diagonal_qber /= v
+                        antidiagonal_qber /= v
+                    break
+                fiber = load_fiber_from_csv(fiber_path)
 
-        # The quantum bits which would have been measured in the wrong base would be disgarded anyway.
-        # Therefore, we can assume that all quantum bits to be considered are those measured in the right base.
-        # We set the angle of the polarization beam splitter to 0° for the horizontal-vertical basis
-        # and to 45° for the diagonal-antidiagonal basis.
-        pbs.rotate(new_transmission_double_theta=0)
-        horizontal_sv_transmitted, horizontal_sv_reflected \
-            = pbs.pass_stokes_vector(fiber.pass_stokes_vector(horizontal_sv))
-        vertical_sv_transmitted, vertical_sv_reflected \
-            = pbs.pass_stokes_vector(fiber.pass_stokes_vector(vertical_sv))
+                # The quantum bits which would have been measured in the wrong base would be disgarded anyway.
+                # Therefore, we can assume that all quantum bits to be considered are those measured in the right base.
+                # We set the angle of the polarization beam splitter to 0° for the horizontal-vertical basis
+                # and to 45° for the diagonal-antidiagonal basis.
+                pbs.rotate(new_transmission_double_theta=0)
+                horizontal_sv_transmitted, horizontal_sv_reflected \
+                    = pbs.pass_stokes_vector(fiber.pass_stokes_vector(horizontal_sv))
+                vertical_sv_transmitted, vertical_sv_reflected \
+                    = pbs.pass_stokes_vector(fiber.pass_stokes_vector(vertical_sv))
 
-        pbs.rotate(new_transmission_double_theta=0.5 * pi)
-        diagonal_sv_transmitted, diagonal_sv_reflected \
-            = pbs.pass_stokes_vector(fiber.pass_stokes_vector(diagonal_sv))
-        antidiagonal_sv_transmitted, antidiagonal_sv_reflected \
-            = pbs.pass_stokes_vector(fiber.pass_stokes_vector(antidiagonal_sv))
+                pbs.rotate(new_transmission_double_theta=0.5 * pi)
+                diagonal_sv_transmitted, diagonal_sv_reflected \
+                    = pbs.pass_stokes_vector(fiber.pass_stokes_vector(diagonal_sv))
+                antidiagonal_sv_transmitted, antidiagonal_sv_reflected \
+                    = pbs.pass_stokes_vector(fiber.pass_stokes_vector(antidiagonal_sv))
 
-        # The quantum bit error rate is the probability that the photon got either transmitted when it should
-        # have been reflected or reflected when it should have been transmitted.
-        # The probability of the photon getting transmitted or reflected is for a single quantum bit the same
-        # thing as the intensity of the transmitted or reflected light pulse in a macroscopic consideration.
-        horizontal_qber += horizontal_sv_reflected.s0 / nr_of_datapoints
-        vertical_qber += vertical_sv_transmitted.s0 / nr_of_datapoints
-        diagonal_qber += diagonal_sv_reflected.s0 / nr_of_datapoints
-        antidiagonal_qber += antidiagonal_sv_transmitted.s0 / nr_of_datapoints
-    horizontal_qbers.append(horizontal_qber)
-    vertical_qbers.append(vertical_qber)
-    diagonal_qbers.append(diagonal_qber)
-    antidiagonal_qbers.append(antidiagonal_qber)
-
-
-bar_width = 0.2
-x = arange(len(combined_fiber_properties))
+                # The quantum bit error rate is the probability that the photon got either transmitted when it should
+                # have been reflected or reflected when it should have been transmitted.
+                # The probability of the photon getting transmitted or reflected is for a single quantum bit the same
+                # thing as the intensity of the transmitted or reflected light pulse in a macroscopic consideration.
+                horizontal_qber += horizontal_sv_reflected.s0
+                vertical_qber += vertical_sv_transmitted.s0
+                diagonal_qber += diagonal_sv_reflected.s0
+                antidiagonal_qber += antidiagonal_sv_transmitted.s0
+                v += 1
+            horizontal_qbers.append(horizontal_qber)
+            vertical_qbers.append(vertical_qber)
+            diagonal_qbers.append(diagonal_qber)
+            antidiagonal_qbers.append(antidiagonal_qber)
+            progress += 1
+            print(f"No compensation fiber analysis progress: {progress}/{max_progress}")
 
 # Create a figure and axis
-fig, ax = plt.subplots(figsize=(20, 10))
+bar_width = 0.2
+horizontal_x = arange(-0.3, max_progress - 0.3, 1)
+vertical_x = arange(-0.1, max_progress - 0.1, 1)
+diagonal_x = arange(0.1, max_progress + 0.1, 1)
+antidiagonal_x = arange(0.3, max_progress + 0.3, 1)
+figure, axes = plt.subplots()
 
 # Plotting the bars for each QBER
-bar1 = ax.bar(x - 1.5 * bar_width, horizontal_qbers, bar_width, label="Horizontal", color="yellow")
-bar2 = ax.bar(x - 0.5 * bar_width, vertical_qbers, bar_width, label="Vertical", color="blue")
-bar3 = ax.bar(x + 0.5 * bar_width, diagonal_qbers, bar_width, label="Diagonal", color="red")
-bar4 = ax.bar(x + 1.5 * bar_width, antidiagonal_qbers, bar_width, label="Antidiagonal", color="green")
+bar1 = axes.bar(horizontal_x, horizontal_qbers, bar_width, label="Horizontal", color="yellow")
+bar2 = axes.bar(vertical_x, vertical_qbers, bar_width, label="Vertical", color="blue")
+bar3 = axes.bar(diagonal_x, diagonal_qbers, bar_width, label="Diagonal", color="red")
+bar4 = axes.bar(antidiagonal_x, antidiagonal_qbers, bar_width, label="Antidiagonal", color="green")
 
-# Adding labels to the x-axis
-ax.set_xticks(x)
+# Adding labels and title with increased font size
+axes.set_xlabel("Fiber Segment counts", fontsize=18)
+axes.set_ylabel("Average expected QBER", fontsize=18)
+axes.set_title("Expected QBERs for Different Segment counts", fontsize=22)
+axes.legend(loc="upper left", fontsize=16)
 
-# Adding labels and title
-ax.set_xlabel("Fiber Segment counts")
-ax.set_ylabel("Average expected QBER")
-ax.set_title("Expected QBERs for Different Segment counts")
-ax.legend()
+# Setting y-axis limits
+axes.set_ylim(0.0, 1.0)
+
+# Adding grid with specified line thicknesses
+# To ensure no grid lines are shown for the x-axis, we set x-axis major and minor grid lines explicitly to False.
+axes.yaxis.grid(True, which="both")
+axes.grid(which="minor", linewidth=0.5, linestyle="-", color="gray", axis="y")
+axes.grid(which="major", linewidth=1.0, linestyle="-", color="black", axis="y")
+
+# Setting minor ticks
+axes.set_yticks([i * 0.1 for i in range(11)], minor=True)
+axes.set_yticks([i * 0.5 for i in range(3)], minor=False)
+
+# Make it so the values on the x-axis are not the same as the progress values, but 1000, 10000, 100000, 1000000.
+# Here we set the x-axis ticks to match the fiber lengths and adjust their labels accordingly.
+axes.set_xticks(arange(max_progress / 8, max_progress, max_progress / 4))
+axes.set_xticklabels([1000, 10000, 100000, 1000000], fontsize=14)
+
+# Increase font size for y-axis tick labels
+axes.tick_params(axis="y", labelsize=14)
 
 # Show the plot
 plt.tight_layout()
--- Analysis/Simulations/precompute_fibers.py
+++ Analysis/Simulations/precompute_fibers.py
@@ -3,37 +3,18 @@ from numpy import pi
 
 theta_fluctuations = [(pi / (2 ** i), i) for i in range(8, 4, -1)]
 delta_fluctuations = [(pi / (2 ** i), i) for i in range(8, 4, -1)]
-max_progress = len(theta_fluctuations) * len(delta_fluctuations) * 220
+segment_counts = [(1000, 100), (10000, 100), (100000, 20), (1000000, 5)]
+max_progress = len(theta_fluctuations) * len(delta_fluctuations) * sum([i for _, i in segment_counts])
 progress = 0
 for theta_fluctuation, i_t in theta_fluctuations:
     for delta_fluctuation, i_d in delta_fluctuations:
-        for i in range(100):
-            segments = 1000
-            fiber = OpticalFiber(
-                nr_of_segments=segments,
-                temporal_pmd_theta_fluctuation=theta_fluctuation,
-                temporal_pmd_delta_fluctuation=delta_fluctuation
-            )
-            save_fiber_to_csv(fiber, f"Fibers/FibersLength1000/fiber_n{segments}_t{i_t}_d{i_d}_v{i}.csv")
-            progress += 1
-            print(f"Progress: {progress}/{max_progress}")
-        for i in range(100):
-            segments = 10000
-            fiber = OpticalFiber(
-                nr_of_segments=segments,
-                temporal_pmd_theta_fluctuation=theta_fluctuation,
-                temporal_pmd_delta_fluctuation=delta_fluctuation
-            )
-            save_fiber_to_csv(fiber, f"Fibers/FibersLength10000/fiber_{segments}_t{i_t}_d{i_d}_v{i}.csv")
-            progress += 1
-            print(f"Progress: {progress}/{max_progress}")
-        for i in range(20):
-            segments = 100000
-            fiber = OpticalFiber(
-                nr_of_segments=segments,
-                temporal_pmd_theta_fluctuation=theta_fluctuation,
-                temporal_pmd_delta_fluctuation=delta_fluctuation
-            )
-            save_fiber_to_csv(fiber, f"Fibers/FibersLength100000/fiber_{segments}_t{i_t}_d{i_d}_v{i}.csv")
-            progress += 1
-            print(f"Progress: {progress}/{max_progress}")
+        for segments, versions in segment_counts:
+            for v in range(versions):
+                fiber = OpticalFiber(
+                    nr_of_segments=segments,
+                    temporal_pmd_theta_fluctuation=theta_fluctuation,
+                    temporal_pmd_delta_fluctuation=delta_fluctuation
+                )
+                save_fiber_to_csv(fiber, f"Fibers/FibersLength{segments}/fiber_{segments}_t{i_t}_d{i_d}_v{v}.csv")
+                progress += 1
+                print(f"Progress: {progress}/{max_progress}")
